ffa28bc81fcfa24e98df054ada12e2f8e570c944
peeratchai kittisupap <peeratck@scg.com>
2020-09-01 13:21:24 +0700

    add login page

---
+++ src/page/Login.js
@@ -0,0 +1,130 @@
+import React, { useState, useRef, useEffect } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: 'auto',
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  header: {
+    marginBottom: '50px',
+    textAlign: 'center'
+  },
+  frame: {
+    border: '1px solid #AEAEAE',
+    padding: '30px'
+  }
+}));
+
+
+
+const SignIn = () => {
+  const classes = useStyles();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const onSignin = () => {
+    if (username.toLowerCase() == "admin" && password.toLowerCase() == "1234") {
+      alert("Sign in successful")
+    } else {
+      alert("Invalid username or password")
+    }
+  }
+
+  const usernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const passwordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h3" className={classes.header}>
+          Bakery
+        </Typography>
+
+
+        <Card className={classes.root}>
+          <CardContent>
+            <Avatar className={classes.avatar}>
+              {/* <LockOutlinedIcon /> */}
+            </Avatar>
+            <Typography component="h1" variant="h5" className={classes.header}>
+              Sign in
+            </Typography>
+            <form className={classes.form} Validate onSubmit={onSignin}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={username}
+                onChange={usernameChange}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={passwordChange}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              // onClick={onSignin}
+              >
+                Sign In
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+
+    </Container>
+  );
+}
+
+export default SignIn;
